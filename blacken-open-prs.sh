69c9570f8a60bc98f419164ba25457e849eab66c
Andy Freeland <andy@goodrx.com>
2018-12-14 11:03:46 -0800

    Also merge the latest BASE_BRANCH when blackening for cleaner diffs

--- blacken-open-prs.sh
+++ blacken-open-prs.sh
@@ -7,13 +7,14 @@ Requires hub and ghi: brew install hub ghi
 Login with hub: hub ci-status
 Login with ghi: ghi config --auth [username]
 
-Usage: $0 REPO PRE_BLACK_COMMIT BLACKENED_COMMIT
+Usage: $0 REPO PRE_BLACK_COMMIT BLACKENED_COMMIT BASE_BRANCH
 
 REPO is the GH org and repo, e.g. GoodRx/test-repo
 PRE_BLACK_COMMIT is the last commit to master/develop that is not formatted with Black
-BLACKEND_COMMIT is the commit that applies Black to the codebase"
+BLACKEND_COMMIT is the commit that applies Black to the codebase
+BASE_BRANCH is the main branch of the repo, e.g. 'master' or 'develop'"
 
-if [ "$#" -ne 3 ]; then
+if [ "$#" -ne 4 ]; then
     echo "${USAGE}"
     exit 1
 fi
@@ -21,6 +22,7 @@ fi
 REPO=$1
 PRE_BLACK_COMMIT="${2}"
 BLACKENED_COMMIT="${3}"
+BASE_BRANCH="${4}"
 
 # First ensure dependencies are installed
 hash hub 2>/dev/null || (echo "Install hub: brew install hub" >&2; exit 1)
@@ -84,6 +86,17 @@ To update your pull request manually, follow these steps:
    \`\`\`
 
 7. Congratulations, you survived the Black migration! :tada:
+
+**NOTE:** The diff for your PR will be very messy at this point. Merge and push the latest \`${BASE_BRANCH}\` into your PR and it will get back to normal.
+EOF
+)
+
+BASE_BRANCH_FAILURE_COMMENT=$(cat <<EOF
+**WARNING:** Your pull-request has a merge conflict with ${BASE_BRANCH}!
+
+Right now, the diff looks very confusing and messy because of the migration to Black. To fix this, checkout your PR, pull the latest changes, and merge the latest ${BASE_BRANCH}, resolving any conflicts.
+
+If you have any questions, join **#python-black-help** on Slack and share your PR.
 EOF
 )
 
@@ -128,6 +141,16 @@ for pr in $(hub pr list --format='%I%n'); do
    fi
 
    # Push the updated PR
-   echo "SUCCESSFULLY UPDATED ${pr}, pushing!"
+   echo "SUCCESSFULLY BLACKENED ${pr}, pushing!"
    git push
+
+   # Merge the latest BASE_BRANCH into the PR to keep the diff clean
+   if ! git merge -q --no-edit "${BASE_BRANCH}"; then
+      echo "${BASE_BRANCH} MERGE FAILED FOR ${pr}"
+         # Add a comment with instructions on merging BASE_BRANCH
+         ghi comment -m "${BASE_BRANCH_FAILURE_COMMENT}" "${pr}"
+   else
+      echo "SUCCESSFULLY MERGED ${BASE_BRANCH} INTO ${pr}, pushing!"
+      git push
+   fi
 done
